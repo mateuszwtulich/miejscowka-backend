bd2ea93296bc5560951719d86ae6a0a06daf81db
MWTULICH <mateusz.wtulich@capgemini.com>
2021-06-07 20:27:27 +0200

    Remote configuration part7

---
+++ target/generated-sources/annotations/com/example/backend/userhandling/logic/api/mapper/AccountMapperImpl.java
@@ -0,0 +1,47 @@
+package com.example.backend.userhandling.logic.api.mapper;
+
+import com.example.backend.userhandling.dataaccess.api.entity.AccountEntity;
+import com.example.backend.userhandling.logic.api.to.AccountEto;
+import com.example.backend.userhandling.logic.api.to.AccountTo;
+import javax.annotation.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-06-07T20:26:45+0200",
+    comments = "version: 1.4.0.Final, compiler: javac, environment: Java 11.0.8 (JetBrains s.r.o.)"
+)
+@Component
+public class AccountMapperImpl implements AccountMapper {
+
+    @Override
+    public AccountEntity toAccountEntity(AccountTo accountTo) {
+        if ( accountTo == null ) {
+            return null;
+        }
+
+        AccountEntity accountEntity = new AccountEntity();
+
+        accountEntity.setPassword( accountTo.getPassword() );
+        accountEntity.setEmail( accountTo.getEmail() );
+
+        return accountEntity;
+    }
+
+    @Override
+    public AccountEto toAccountEto(AccountEntity accountEntity) {
+        if ( accountEntity == null ) {
+            return null;
+        }
+
+        AccountEto accountEto = new AccountEto();
+
+        accountEto.setId( accountEntity.getId() );
+        accountEto.setUsername( accountEntity.getUsername() );
+        accountEto.setPassword( accountEntity.getPassword() );
+        accountEto.setEmail( accountEntity.getEmail() );
+        accountEto.setActivated( accountEntity.isActivated() );
+
+        return accountEto;
+    }
+}
---
+++ target/generated-sources/annotations/com/example/backend/userhandling/logic/api/mapper/UserMapperImpl.java
@@ -0,0 +1,45 @@
+package com.example.backend.userhandling.logic.api.mapper;
+
+import com.example.backend.userhandling.dataaccess.api.entity.UserEntity;
+import com.example.backend.userhandling.logic.api.to.UserEto;
+import com.example.backend.userhandling.logic.api.to.UserTo;
+import javax.annotation.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-06-07T20:26:45+0200",
+    comments = "version: 1.4.0.Final, compiler: javac, environment: Java 11.0.8 (JetBrains s.r.o.)"
+)
+@Component
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public UserEntity toUserEntity(UserTo userTo) {
+        if ( userTo == null ) {
+            return null;
+        }
+
+        UserEntity userEntity = new UserEntity();
+
+        userEntity.setName( userTo.getName() );
+        userEntity.setSurname( userTo.getSurname() );
+
+        return userEntity;
+    }
+
+    @Override
+    public UserEto toUserEto(UserEntity userEntity) {
+        if ( userEntity == null ) {
+            return null;
+        }
+
+        UserEto userEto = new UserEto();
+
+        userEto.setId( userEntity.getId() );
+        userEto.setName( userEntity.getName() );
+        userEto.setSurname( userEntity.getSurname() );
+
+        return userEto;
+    }
+}
