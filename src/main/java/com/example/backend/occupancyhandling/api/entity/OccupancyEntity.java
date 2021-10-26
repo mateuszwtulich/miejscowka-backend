3d5bfa3e355bf052b00ff82cc93b2e260ab995c0
Filip <floffler@wp.pl>
2021-10-27 00:13:48 +0200

    OccupancyEntity Table name fix typo

--- src/main/java/com/example/backend/occupancyhandling/api/entity/OccupancyEntity.java
+++ src/main/java/com/example/backend/occupancyhandling/api/entity/OccupancyEntity.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Entity
-@Table(name = "OCCUMPANCY")
+@Table(name = "OCCUPANCY")
 @IdClass(OccupancyId.class)
 public class OccupancyEntity {
 
